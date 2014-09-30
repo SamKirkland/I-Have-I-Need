9cb14190a5061cd2f79ca73ef7a4ab79a6fb0f74
SamKirkland <github@samkirkland.com>
2014-09-30 12:50:19 -0500

    Project Creation

---
+++ Main/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Main.Startup))]
+namespace Main
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
