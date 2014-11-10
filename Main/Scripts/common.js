28948d171bc470e66aa6d97f2026fa37e006e034
SamKirkland <github@samkirkland.com>
2014-11-09 18:29:20 -0600

    Derek - integrated theme

---
+++ Main/Scripts/common.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+    $(window).scroll(function (scroll) {
+        var pxFromTop = $(document).scrollTop();
+        var newSize;
+        
+        if (pxFromTop > 110) {
+            pxFromTop = 110;
+        }
+
+        newSize = 200 - pxFromTop;
+        //newMargin = 300 - pxFromTop*2.7272727272;
+
+
+        $("#logo").css({
+            width: newSize,
+            height: newSize
+        });
+
+        //$("#pageTitle").css({
+        //    marginLeft: newMargin
+        //});
+    });
+
+
+});
